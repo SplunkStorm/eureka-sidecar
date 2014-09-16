d7caf39aed936183af3cd9e2d9416b0767446583
Matt Harris <matthaeus.harris@gmail.com>
2014-09-16 14:01:33 -0700

    Change package.json format to match npm specs

--- bin/cli.js
+++ bin/cli.js
@@ -3,12 +3,12 @@
 var sidecar = require('../lib/eureka-sidecar');
 
 sidecar.initialize({
-	'eureka_url': 'http://ec2-54-165-101-15.compute-1.amazonaws.com/eureka',
+	'eureka_url': process.env.EUREKA_URL || 'http://ec2-54-165-101-15.compute-1.amazonaws.com/eureka',
 	'app': process.env.APP || 'sidecar',
-	'ip-address': '10.13.6.48',
-	'app-port': '5000',
+	'ip-address': process.env.IP_ADDRESS || '10.13.6.48',
+	'app-port': process.env.PORT || '5000',
 	'hostname': process.env.HOSTNAME || 'frankenstein',
 	'metadata': {
-		'version': '0.0.1'
+		'version': process.env.VERSION || '0.0.1'
 	}
 });
