aa9c6272886c4aede6394a7505954e1475e3d735
Matt Harris <matthaeus.harris@gmail.com>
2014-09-24 15:17:07 -0700

    AMI-396 add saner defaults to sidecar

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
-
+var os = require('os');
+var fs = require('fs');
 var sidecar = require('../lib/eureka-sidecar');
+var buildNumber = -1
 
-sidecar.initialize({
-	'eureka_url': process.env.EUREKA_URL || 'http://ec2-54-165-101-15.compute-1.amazonaws.com/eureka',
-	'app': process.env.APP || 'sidecar',
-	'ip-address': process.env.IP_ADDRESS || '10.13.6.48',
-	'app-port': process.env.PORT || '5000',
-	'hostname': process.env.HOSTNAME || 'frankenstein',
-	'metadata': {
-		'version': process.env.VERSION || '0.0.1'
+fs.readFile('/etc/buildnumber', function(err, data) {
+	if (!err) {
+		buildNumber = data.trim();
 	}
+
+	sidecar.initialize({
+		'eureka_url': process.env.EUREKA_URL || 'http://eureka.services.splunkcloud.net/eureka',
+		'app': process.env.APP || 'sidecar',
+		'ip-address': process.env.IP_ADDRESS || os.networkInterfaces().eth0[0].address,
+		'app-port': process.env.PORT || '5000',
+		'hostname': process.env.HOSTNAME || os.hostname(),
+		'metadata': {
+			'version': process.env.VERSION || '0.0.1',
+			'build': 
+		}
+	});
 });
+
